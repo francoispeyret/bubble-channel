08b9f290958af29d2e33bbd5c7edbcea517c997e
fpeyret <peyretfr@gmail.com>
2022-07-02 00:12:23 +0200

    gitignore

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
 
-
 module.exports = class BubbleChannelPlugin extends Plugin {
     startPlugin() {
-
+        alert('aaa');
     }
 };
